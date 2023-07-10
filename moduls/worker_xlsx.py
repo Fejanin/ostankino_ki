84437782933c129d42fdec3d215b396ab99222eb
Shvec Sergei <fejanin@mail.ru>
2023-07-10 17:35:12 +0300

    Теперь можно работать с данными из 1С

--- moduls/worker_xlsx.py
+++ moduls/worker_xlsx.py
@@ -34,9 +34,10 @@ class XLSX_Data:
 
 
     def __str__(self):
-        return f'{self.num_row}){self.product}: {self.num_order}.'
+        return f'Строка - {self.num_row}; наименование - {self.product}.'
 
 
+    '''
     def __eq__(self, obj):
         if type(self) == type(obj):
             return self.sales_unit_code == obj.sales_unit_code and self.product_code == obj.product_code and self.option_number == obj.option_number and self.barcode == obj.barcode
@@ -44,11 +45,11 @@ class XLSX_Data:
             # TODO
             # сравнение с другими объектами
             return False
+    '''
 
 
-    def __hash__(self):
-        '''пока не используем'''
-        return hash((self.sales_unit_code, self.product_code, self.option_number, self.barcode))
+    def __eq__(self, obj):
+        return self.sales_unit_code == obj.sales_unit_code and self.product_code == obj.product_code and self.option_number == obj.option_number and self.barcode == obj.barcode
 
 
     def __setattr__(self, key, value):
@@ -61,6 +62,7 @@ class XLSX_Data:
     @classmethod
     def control_line(cls, data):
         return all(data[cls.START_SLICE: cls.END_SLICE]) and data[cls.CONTROL_KG] == 'кг'
+#===========================================================================================
 
 
 class POKOM_Reader:
@@ -83,45 +85,139 @@ class POKOM_Reader:
 
     def __call__(self):
         return round(sum([i.num_order for i in self.all_rows]), 3)
+#===========================================================================================
+
+
+class OneC:
+    def __init__(self, file):
+        self.wb = openpyxl.load_workbook(file)
+        self.ws = self.wb.active
+        self.keys_values = self.read()
+        self.translater_data = self.find_keys()
+        self.all_rows = self.translater()  # хранит список всех объектов OneCData
+        
+
+    def read(self):
+        '''Считывает данные из файла, хранящего заказ. Наименования должны соответствовать СКЮ 1С!!!
+Получает следующие данные:
+    - Название СКЮ (в дальнейшем будет ключом);
+        - количесто шт/кг из заказа
+        - номер строки.'''
+        a = self.ws.iter_rows(1, self.ws.max_row)
+        res = {}
+        for num, i in enumerate(a, 1):
+            if i:
+                data = list(map(lambda x: x.value if type(x.value) in (int, float) else x.value.strip() if type(x.value) is str else None, i))
+                if data[1] and data[1] > 0:
+                    res[data[0].strip()] = {'value': data[1], 'row': num}
+        return res
+
+
+    def find_keys(self):
+        file = r'moduls\1С.xlsx' # файл с ключами для поиска
+        wb = openpyxl.load_workbook(file)
+        ws = wb.active
+        a = ws.iter_rows(1, ws.max_row + 1)
+        res = {}
+        for i in a:
+            time_data = list(map(lambda x: x.value, i))
+            if time_data[0] and time_data[0] != '1С':
+                res[time_data[0].strip()] = time_data[1:5]
+        return res
+
+
+    def translater(self):
+        res = []
+        for i in self.keys_values:
+            if i in self.translater_data:
+                res.append(OneCData(self.translater_data[i] + [i, self.keys_values[i]['value']], self.keys_values[i]['row']))
+            else:
+                res.append(OneCData([None for _ in range(4)]+ [i, self.keys_values[i]['value']], self.keys_values[i]['row']))
+
+        return res
+
+
+    def __call__(self):
+        return round(sum([i.num_order for i in self.all_rows]), 3)
+
+
+class OneCData:
+    def __init__(self, data, number):
+        self.sales_unit_code, self.product_code, self.option_number, self.barcode, self.product, self.num_order = data
+        self.num_row = number
+
+
+    def __str__(self):
+        return f'Строка - {self.num_row}; наименование - {self.product}.'
+
+
+    def __eq__(self, obj):
+        return self.sales_unit_code == obj.sales_unit_code and self.product_code == obj.product_code and self.option_number == obj.option_number and self.barcode == obj.barcode
+
+
+    def __setattr__(self, key, value):
+        '''Удаляем пробелы из начала и окончания строки.'''
+        if type(value) is str:
+            value = value.strip()
+        object.__setattr__(self, key, value)
+#===========================================================================================
 
 
 class POKOM_Rewriter:
-    def __init__(self, read_name_file, write_name_file, flag_pocom = True):
-        if flag_pocom: # файл ПОКОМ
+    def __init__(self, read_name_file, write_name_file, flag_pocom = False):
+        self.tracker = Tracker()
+        if not flag_pocom: # файл ПОКОМ
             self.read_file = POKOM_Reader(read_name_file)
         else:
+            if flag_pocom == '1С': # "С" - символ кирилицей!!!!
+                self.read_file = OneC(read_name_file)
             # TODO
             # выбрать клиента, для использования соответствующего класса
             pass
         self.write_file = POKOM_Reader(write_name_file)
         for i in self.read_file.all_rows:
             flag = False
+            obj1 = str(i)
             for j in self.write_file.all_rows:
+                obj2 = str(j)
                 if i == j and i.num_order:
                     flag = True
-                    #XLSX_Writer(self.write_file.ws, j, i.num_order)
                     self.write(j, i.num_order)
-                    # TODO
-                    # добавить в треккер изменения (Tracker)
+                    self.tracker.message.append(f'Перенос данных из {obj1} ==> {obj2}, в количестве {i.num_order}')
+                    self.tracker.wight2 += i.num_order
                     continue
             if not flag and i.num_order: # ТРЕБУЕТСЯ ПРОВЕРИТЬ ПРАВИЛЬНОСТЬ ПРОВЕРКИ!!!
-                # TODO
-                # добавить в треккер ошибку (Tracker)
-                print('NOT FOUND')
-    # СОХРАНИТЬ изменени в файлах
+                self.tracker.error.append(f'Объект - {obj1} в количестве {i.num_order} не найден.')
         self.read_file.wb.save(read_name_file)
         self.write_file.wb.save(write_name_file)
+        self.tracker.wight1 = self.read_file()
+        self.tracker.create_file()
 
 
     def write(self, obj, num):
         self.write_file.ws[obj.NAME_COL + str(obj.num_row)] = num
 
 
-class XLSX_Writer:
-    def __init__(self, sheet, obj_to, num_order):
-        sheet[obj_to.NAME_COL + str(obj_to.num_row)] = num_order
-
-
 class Tracker:
     '''Создает текстовый файл с результатами переноса данных (в т.ч. и ошибками)'''
-    pass
+    def __init__(self):
+        self.error = []
+        self.message = []
+        self.wight1 = 0
+        self.wight2 = 0
+
+
+    def create_file(self):
+        with open('REPORT.txt', 'w') as f:
+            if self.error:
+                f.write(f'Обнаружены следующие ошибки:\n')
+                for i in self.error:
+                    f.write(i + '\n')
+                f.write('#' * 50 + '\n\n')
+            for i in self.message:
+                f.write(i + '\n')
+            f.write(f'\nВес продукции из первого файла: {self.wight1}\n')
+            f.write(f'Вес продукции во втором фале составляет: {round(self.wight2, 3)}\n')
+
+
+#file = OneC('перенос данных из 1с.xlsx')
